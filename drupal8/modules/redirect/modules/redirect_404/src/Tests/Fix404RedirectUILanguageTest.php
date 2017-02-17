176efabc7f93f2a792eedb6e7cf82ea0b734eca4
Dave Lane <dave@oerfoundation.org>
2017-02-17 00:50:28 +0000

    update to 8.2.6 and a bunch of modules

--- drupal8/modules/redirect/modules/redirect_404/src/Tests/Fix404RedirectUILanguageTest.php
+++ drupal8/modules/redirect/modules/redirect_404/src/Tests/Fix404RedirectUILanguageTest.php
@@ -24,7 +24,7 @@ class Fix404RedirectUILanguageTest extends Redirect404TestBase {
    *
    * @var array
    */
-  public static $modules = ['content_translation'];
+  public static $modules = ['language'];
 
   /**
    * Admin user's permissions for this test.
@@ -33,6 +33,7 @@ class Fix404RedirectUILanguageTest extends Redirect404TestBase {
    */
   protected $adminPermissions = [
     'administer redirects',
+    'administer redirect settings',
     'access site reports',
     'access content',
     'bypass node access',
